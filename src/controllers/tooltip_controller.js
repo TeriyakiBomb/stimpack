0e828fc94b721eccda292a4d215f7a3a91f679f7
Ash Joseph <kamikazedesigns@gmail.com>
2024-10-20 16:20:30 +0100

    Basic implementation based on demo works

---
+++ src/controllers/tooltip_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus"
+import { computePosition, arrow, offset } from "@floating-ui/dom"
+
+export default class extends Controller {
+  static targets = ["popover", "arrow"]
+
+  static values = {
+    position: { type: String, default: "bottom" },
+  }
+
+  toggle() {
+    const popover = this.popoverTarget
+    popover.style.display = popover.style.display === "none" ? "block" : "none"
+    if (popover.style.display === "block") {
+      this.updatePosition()
+    }
+  }
+
+  updatePosition() {
+    const button = this.element
+    const popover = this.popoverTarget
+    const arrowElement = this.arrowTarget
+
+    computePosition(button, popover, {
+      placement: this.positionValue,
+      middleware: [arrow({ element: arrowElement }), offset(20)],
+    }).then(({ x, y, placement, middlewareData }) => {
+      Object.assign(popover.style, {
+        left: `${x}px`,
+        top: `${y}px`,
+      })
+
+      const { x: arrowX, y: arrowY } = middlewareData.arrow
+
+      const staticSide = {
+        top: "bottom",
+        right: "left",
+        bottom: "top",
+        left: "right",
+      }[placement.split("-")[0]]
+
+      Object.assign(arrowElement.style, {
+        left: arrowX != null ? `${arrowX}px` : "",
+        top: arrowY != null ? `${arrowY}px` : "",
+        right: "",
+        bottom: "",
+        [staticSide]: "-8px",
+      })
+    })
+  }
+}
