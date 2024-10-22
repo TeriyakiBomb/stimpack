9c103ca6c91e58d4b9b1335e17a241a65554ab63
Ash <kamikazedesigns@gmail.com>
2024-10-22 20:48:26 +0100

    Added margin and swatch values

--- src/controllers/color-picker_controller.js
+++ src/controllers/color-picker_controller.js
@@ -4,18 +4,25 @@ import Coloris from "@melloware/coloris"
 // Connects to data-controller="color-picker"
 export default class extends Controller {
   static targets = ["input"]
+  static values = {
+    swatches: Array,
+    margin: { type: Number, default: 12 },
+  }
 
   connect() {
-    // Initialize Coloris
     Coloris.init()
-    Coloris({ el: this.inputTarget })
-    console.log("red")
+    Coloris({
+      el: this.inputTarget,
+      swatches: this.swatchesValue,
+      margin: this.marginValue,
+      theme: "polaroid",
+    })
+    console.log(this.swatchesValue)
   }
 
   handleColorChange(event) {
     const color = event.target.value
     console.log(`Selected color: ${color}`)
-    // You can add additional functionality here, like updating other elements
   }
 
   disconnect() {
